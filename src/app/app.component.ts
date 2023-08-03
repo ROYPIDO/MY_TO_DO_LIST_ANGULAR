924c0c76a6e279e9c89be38a997c4cf31b9d83ae
ROYPIDO <roypido@gmail.com>
2023-08-03 11:29:54 +0200

    function display added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
-import { Model } from './model';
+import { Model, ToDoItem } from './model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-      model = new Model();
-  title: any;
+  model = new Model();
+  //title: any;
+  isDisplay = false;
 
-      getName(): string {
-        return this.model.user;
-}
+  getName(): string {
+    return this.model.user;
+  }
 
-getItems(): Array<any> {
-  return this.model.items;
-}
+  getItems(): Array<any> {
+    if(this.isDisplay){
+      return this.model.items;
+    }
+    return this.model.items.filter((item) => !item.action);
+  }
 
+  addItem(value: any) {
+    if (value != '') {
+      this.model.items.push(new ToDoItem(value, false));
+    }
+  }
 }
